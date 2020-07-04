6d3ffe7ee7161cfbcf2bcc884f9feeeef0deb680
arun-moses <63541629+arun-moses@users.noreply.github.com>
2020-07-04 17:47:39 +0530

    updatted for graadle
    updatted for graadle

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=H:\Android\flutter-SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=H:\Projects-Flutter\flutter_interview_test"
+export "FLUTTER_APPLICATION_PATH=H:\Projects-Flutter\FlutterInterview"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
